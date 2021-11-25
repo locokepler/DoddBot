0cc4be02bd6c26d1ff8d15b90e3ad742c64fe149
locokepler <locokepler@gmail.com>
2021-11-25 12:18:19 -0600

    changes to how the board is defined and some constants

--- chess_rules.h
+++ chess_rules.h
@@ -19,20 +19,29 @@
 #define K_LFT_DN -10
 #define K_LFT_UP 6
 
+#define W_K_CASTLE 0x1
+#define W_Q_CASTLE 0x2
+#define B_K_CASTLE 0x4
+#define B_Q_CASTLE 0x8
+
 /* time to define what a board is. It is a 64 array of chars, corrosponding
  * to the positions on the board. 0 is (a, 1),  34 is (e, 3), etc. Also
  * includes a flag for which player's turn it is. White is a 0, Black is a 1
  * 
  * Piece definitions: PNBRQK, uppercase is white
  * 
- * en passant is given as the imagined location of the pawn that could be
- * enpassanted. For any pawn pushed two places it is at the location as if it
- * had only moved one place.
+ * en passant is given as the en passant target square
  * 
  * Also includes a set of flags.
  *  List of flags:
- *      * 0x1: can white castle
- *      * 0x2: can black castle
+ *      * 0x1: white can castle kingside
+ *      * 0x2: white can castle queenside
+ *      * 0x4: black can castle kingside
+ *      * 0x8: black can castle queenside
+ * 
+ * The array of pieces is setup with the piece char on the even indeces and the
+ * location of that piece on the following odd index. Ex. b_pieces[0] == 'K',
+ * w_pieces[1] == 59
  */
 
 typedef struct board_ {
@@ -42,21 +51,9 @@ typedef struct board_ {
     int enpassant;
     int white_attacking[2];
     int black_attacking[2];
-    pieces white_pieces;
-    pieces black_pieces;
+    char w_pieces[32];
+    char b_pieces[32];
 } board;
-
-typedef struct pieces_ {
-    int king;
-    int queen;
-    int rook_a;
-    int rook_h;
-    int knight_b;
-    int knight_g;
-    int bishop_c;
-    int bishop_f;
-    int pawns[8];
-} pieces;
 
 // definition of a move. Is from location to location. a->b. Includes which
 // player is making the move. 
